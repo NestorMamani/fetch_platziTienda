6b27960ac01e4fcd6c85ef50ee6a9f85b0a59a51
Nestor Mamani <nmamanipantoja@gmail.com>
2021-04-19 11:39:29 -0500

    v1.0.0

--- src/index.js
+++ src/index.js
@@ -3,4 +3,39 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = 'https://platzi-avo.vercel.app/api/avo';
+const container = document.querySelector('div#app');
+
+let nodes = [];
+
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat('en-EN', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price);
+
+  return newPrice;
+};
+
+fetch(url)
+  .then((response) => response.json()) //Procesar rpta y convertir a json
+  .then((responseJSON) => {
+    responseJSON.data.forEach((item) => {
+      const element = document.createElement('div');
+      element.classList.add('item');
+      const title = document.createElement('h1');
+      const image = document.createElement('img');
+      const text = document.createElement('h4');
+
+      title.innerText = item.name;
+      title.className = 'title';
+      image.src = 'https://platzi-avo.vercel.app' + item.image;
+      text.innerText = formatPrice(item.price);
+
+      element.append(title, image, text);
+
+      nodes.push(element);
+    });
+
+    container.append(...nodes);
+  }); //render data
